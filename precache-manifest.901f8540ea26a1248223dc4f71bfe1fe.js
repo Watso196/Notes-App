c9a3d2d4e5a1f148a3c9054ee897a8b48f9a830a
Watso196 <watso196@msu.edu>
2020-08-24 16:12:18 -0400

    Updates

---
+++ precache-manifest.901f8540ea26a1248223dc4f71bfe1fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8a43431a09a036374e9709bde3f821",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "f2778cde2ee1f3b204e3",
+    "url": "/Notes-App/static/css/main.0872b215.chunk.css"
+  },
+  {
+    "revision": "87778e728c84874339e1",
+    "url": "/Notes-App/static/js/2.bd14f541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-App/static/js/2.bd14f541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2778cde2ee1f3b204e3",
+    "url": "/Notes-App/static/js/main.6215210b.chunk.js"
+  },
+  {
+    "revision": "26c851cd27d9dece27b7",
+    "url": "/Notes-App/static/js/runtime-main.083ebf96.js"
+  },
+  {
+    "revision": "267ddbbe5200f40feae1ff77077c5a36",
+    "url": "/Notes-App/static/media/delete.267ddbbe.svg"
+  }
+]);
